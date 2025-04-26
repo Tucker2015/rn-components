9e53aa55d61f2c36bb3a9c812351d5ff14f36dc0
Tucker2015 <kevtucker@gmail.com>
2025-04-26 06:56:19 +0100

    feat: add Lato font support to App component and update dependencies

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,6 +4,11 @@ import {
   Inter_900Black,
   useFonts,
 } from '@expo-google-fonts/inter';
+import {
+  Lato_400Regular,
+  Lato_700Bold,
+  Lato_900Black,
+} from '@expo-google-fonts/lato';
 import Homescreen from './Homescreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
@@ -13,6 +18,9 @@ export default function App() {
     Inter: Inter_400Regular,
     InterSemibold: Inter_600SemiBold,
     InterBlack: Inter_900Black,
+    Lato: Lato_400Regular,
+    LatoBold: Lato_700Bold,
+    LatoBlack: Lato_900Black,
   });
   if (!fontsLoaded) {
     return null;
